3a355667e849b6969eeff9369e50b5fa9de9d684
anyamilichenko <annaburmatova395@gmail.com>
2025-12-05 21:02:43 +0300

    6

--- src/main/java/ru/itmo/SoaServiceBApplication.java
+++ src/main/java/ru/itmo/SoaServiceBApplication.java
@@ -2,9 +2,16 @@ package ru.itmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SoaServiceBApplication {
+public class SoaServiceBApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SoaServiceBApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SoaServiceBApplication.class, args);
     }
